99d25b2603a3aa9e95916521471b494ec9d36a35
Jarek Radosz <jradosz@gmail.com>
2017-02-08 18:32:07 +0100

    Remove placeholder spec.

--- spec/letsencrypt_heroku_spec.rb
+++ spec/letsencrypt_heroku_spec.rb
@@ -4,8 +4,4 @@ describe LetsencryptHeroku do
   it 'has a version number' do
     expect(LetsencryptHeroku::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
