95ef8b6c9e1bc93232abfd8ce074b3a6ac352475
Jarek Radosz <jradosz@gmail.com>
2017-09-22 10:44:20 +0200

    Add keep_certs config option.

--- lib/letsencrypt_heroku/process/update_certificates.rb
+++ lib/letsencrypt_heroku/process/update_certificates.rb
@@ -8,15 +8,20 @@ class LetsencryptHeroku::Process
       output 'Update certificates' do
         csr = Acme::Client::CertificateRequest.new(names: context.config.domains)
         certificate = context.client.new_certificate(csr)
-        File.write('privkey.pem', certificate.request.private_key.to_pem)
-        File.write('fullchain.pem', certificate.fullchain_to_pem)
+        privkey_name = "privkey_#{herokuapp}.pem"
+        fullchain_name = "fullchain_#{herokuapp}.pem"
+        File.write(privkey_name, certificate.request.private_key.to_pem)
+        File.write(fullchain_name, certificate.fullchain_to_pem)
 
         if has_already_cert(herokuapp)
-          execute "heroku certs:update fullchain.pem privkey.pem --confirm #{herokuapp} --app #{herokuapp}"
+          execute "heroku certs:update #{fullchain_name} #{privkey_name} --confirm #{herokuapp} --app #{herokuapp}"
         else
-          execute "heroku certs:add fullchain.pem privkey.pem --app #{herokuapp}"
+          execute "heroku certs:add #{fullchain_name} #{privkey_name} --app #{herokuapp}"
         end
-        FileUtils.rm %w(privkey.pem fullchain.pem)
+
+        unless context.config.keep_certs
+          FileUtils.rm [privkey_name, fullchain_name]
+        end
       end
       puts # finish the output with a nice newline ;)
     end
